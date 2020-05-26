be756cdc9111be1b2ad702f0b567778afe8cb1e2
Kuniyoshi Kenichiro <kuni.0518planetarium@gmail.com>
2020-05-27 00:30:15 +0900

    add sample1.py

---
+++ sample1.py
@@ -0,0 +1,14 @@
+print("hello world")
+
+test_set = {"abc", "def"}
+test_set.add("ghi")
+
+print(test_set)
+
+
+a = [i for i in range(10)]
+print(a)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# a[start:end:step]
+print(a[3:7])  # [3, 4, 5, 6]
+print(a[::2])  # [0, 2, 4, 6, 8]
